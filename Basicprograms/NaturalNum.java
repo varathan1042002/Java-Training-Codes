9bc649d1c5972143ed8c1de90ba3778f544df76f
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-30 10:40:15 +0530

    Create NaturalNum.java

---
+++ Basicprograms/NaturalNum.java
@@ -0,0 +1,23 @@
+
+public class Natural_Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		int sum=0;
+		/*for(int i=1;i<=n;n=n-1) {
+			sum=sum+n;
+			
+			
+		/}
+		//while(n>0)
+		//{
+		//	sum=sum+n;
+		//	n=n-1;
+		//}*/
+		sum=(n*(n+1))/2;
+		System.out.print(sum);
+
+	}
+
+}
