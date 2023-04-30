d135c5e04b9da5dec3e8390d3b785667f9d1f677
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-30 11:14:49 +0530

    Create ArrayAscending.java

---
+++ Arrays/ArrayAscending.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Ascending {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		//int n=scan.nextInt();
+		Ascending ac = new Ascending();
+		//ac.process(n);
+		int arr[] = {10,20,76,86};
+		
+		for(int i=arr.length-1;0<i;i--) {
+			System.out.print(arr[i]);
+		}
+	}
+
+}
